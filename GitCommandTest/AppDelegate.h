7b309b4a26d9de6d6ba177fdaca76724fa7c5ac1
liem <liem@liemdeMBP.lan>
2017-11-15 14:13:08 +0800

    first commit

---
+++ GitCommandTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitCommandTest
+//
+//  Created by liem on 2017/11/15.
+//  Copyright © 2017年 liem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitCommandTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitCommandTest
+//
+//  Created by liem on 2017/11/15.
+//  Copyright © 2017年 liem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
